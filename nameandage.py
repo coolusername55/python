27f3a31b25fcab234140c87efd169cecbfb834ac
coolusername55 <113976984+coolusername55@users.noreply.github.com>
2022-12-13 09:44:36 +0000

    Add files via upload

---
+++ nameandage.py
@@ -0,0 +1,17 @@
+Forename = "Kurtis"
+Surname = "Cousins"
+
+Age = 83
+
+Fullname = Forename + " " + Surname
+
+print("Your first name is " + Forename)
+print("Your surname is " + Surname)
+
+print("This year you are " + str(Age) + " years old.")
+
+Age += 1
+
+print("Next year you will be " + str(Age) + " years old")
+
+print("End of application.....")
